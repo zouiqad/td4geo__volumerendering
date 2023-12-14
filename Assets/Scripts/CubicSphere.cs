c768dded42048e000f870dc4f375f02472845fe3
weiss <lebsaira.wassil@gmail.com>
2023-12-14 06:56:19 +0100

    added bounding box

--- Assets/Scripts/CubicSphere.cs
+++ Assets/Scripts/CubicSphere.cs
@@ -1,65 +1,100 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class CubicSphere : MonoBehaviour
 {
-    public GameObject cubePrefab;
-    public float radius = 10.0f;
-    public Vector3 center = Vector3.zero;
-    public int subDivision = 10;
+    [System.Serializable]
+    public class PotentialCube
+    {
+        public GameObject cubeObject;
+        public float potential;
+    }
+    public float threshold = 0.5f;
+    public float initialPotential = 1.0f; // Potentiel initial pour la sphère
+    public List<PotentialCube> cubes;
 
-    private float lastRadius;
-    private int lastSubDiv;
-    private Vector3 lastCenter;
+    [System.Serializable]
+    public class SphereData
+    {
+        public Vector3 position;
+        public float radius;
+    }
+
+    public enum OperationType { Union, Intersection }
+
+    public List<SphereData> spheres;
+    public GameObject boundingBoxPrefab;
+    public GameObject cubePrefab;
+    public float cubeSize = 1f; // Taille fixée par l'utilisateur
+    public OperationType operation = OperationType.Union;
+
+
     void Start()
     {
-        lastRadius = radius;
-        lastSubDiv = subDivision;
-        lastCenter = center;
-        GenerateCubicSphere(center, radius, subDivision);
+        GenerateVolume();
     }
 
-    void Update()
+    
+
+    void GenerateVolume()
     {
-        if (lastRadius != radius || lastSubDiv != subDivision
-            || lastCenter != center)
+        Vector3 minPoint = Vector3.positiveInfinity;
+        Vector3 maxPoint = Vector3.negativeInfinity;
+
+        // Calcul de la boîte englobante
+        foreach (SphereData sphere in spheres)
         {
-            DeleteCubicSphere();
-            GenerateCubicSphere(center, radius, subDivision);
-            lastRadius = radius;
-            lastSubDiv = subDivision;
-            lastCenter = center;
+            minPoint = Vector3.Min(minPoint, sphere.position - Vector3.one * sphere.radius);
+            maxPoint = Vector3.Max(maxPoint, sphere.position + Vector3.one * sphere.radius);
         }
-        
-    }
 
-    void DeleteCubicSphere()
-    {
-        foreach(Transform child in transform)
-        {
-            Destroy(child.gameObject);
-        }
-    }
-    void GenerateCubicSphere(Vector3 center, float radius, int subDivision)
-    {
-        float voxelSize = 2.0f * radius / subDivision;
+        Vector3 BoundingBoxCenter = (minPoint + maxPoint) / 2;
+        Vector3 BoundingBoxSize = maxPoint - minPoint;
 
-        for (float x = center.x - radius; x <= center.x + radius; x += voxelSize)
+        int gridSizeX = Mathf.CeilToInt(BoundingBoxSize.x / cubeSize);
+        int gridSizeY = Mathf.CeilToInt(BoundingBoxSize.y / cubeSize);
+        int gridSizeZ = Mathf.CeilToInt(BoundingBoxSize.z / cubeSize);
+
+
+        // Instantiation du cube qui represente la boîte englobante
+        GameObject boundingBox = Instantiate(boundingBoxPrefab, BoundingBoxCenter, Quaternion.identity);
+        boundingBox.transform.localScale = BoundingBoxSize;
+
+        // Création des spheres
+        for (int x = 0; x < gridSizeX; x++)
         {
-            for (float y = center.y - radius; y <= center.y + radius; y += voxelSize)
+            for (int y = 0; y < gridSizeY; y++)
             {
-                for (float z = center.z - radius; z <= center.z + radius; z += voxelSize)
+                for (int z = 0; z < gridSizeZ; z++)
                 {
-                    float distance = Mathf.Sqrt(x * x + y * y + z * z);
-
-                    if (distance <= radius)
+                    Vector3 pos = minPoint + new Vector3(x, y, z) * cubeSize;
+                    if (IsInsideSpheres(pos))
                     {
-                        Vector3 position = new Vector3(x, y, z);
-                        GameObject cube = Instantiate(cubePrefab, position, Quaternion.identity);
-                        cube.transform.localScale = new Vector3(voxelSize, voxelSize, voxelSize);
-                        cube.transform.SetParent(transform);
+                        GameObject voxelGO = Instantiate(cubePrefab, pos, Quaternion.identity);
+                        voxelGO.transform.parent = boundingBox.transform;
                     }
                 }
             }
         }
     }
+
+    bool IsInsideSpheres(Vector3 point)
+    {
+        bool insideAnySphere = false;
+        bool insideAllSpheres = true;
+
+        foreach (SphereData sphere in spheres)
+        {
+            float distanceToCenter = (sphere.position - point).magnitude;
+            bool insideThisSphere = distanceToCenter < sphere.radius;
+
+            if (insideThisSphere)
+                insideAnySphere = true;
+            else
+                insideAllSpheres = false;
+        }
+
+        return operation == OperationType.Union ? insideAnySphere : insideAllSpheres;
+    }
+
 }
